9b9e900a47bf06ffeacba56b11bef9d9fe698322
hcampos <hcampospro@gmail.com>
2023-03-29 11:19:13 +0200

    🏷️ Update types

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ const endtime = <HTMLParagraphElement>document.querySelector('.endtime')
 const music = <HTMLAudioElement>document.querySelector('#music')
 
 /**Variables */
-let index = 0
+let index: number = 0
 
 /**Function to init the project */
 const initProject = (index: number): void => {
@@ -33,7 +33,7 @@ const initProject = (index: number): void => {
 }
 
 /**Function to change the data on the init and when we click on forward or backward */
-const changeData = (index: number) => {
+const changeData = (index: number): void => {
   music_player.style.background = `linear-gradient(45deg, ${artists[index].primary} 27%, ${artists[index].secondary} 72%)`
   title.innerHTML = artists[index].title
   artist.innerHTML =
@@ -50,37 +50,37 @@ const changeData = (index: number) => {
 initProject(index)
 
 /**When the music is loaded get the duration */
-music.onloadedmetadata = () => {
+music.onloadedmetadata = (): void => {
   const splitDuration = (music.duration / 60).toFixed(2).split('.')
   endtime.innerHTML = '0' + splitDuration[0] + ' : ' + splitDuration[1]
 }
 
 /**When the time of the music change update the music slider */
-music.ontimeupdate = () => {
+music.ontimeupdate = (): void => {
   if (music.currentTime) {
     duration.value = ((music.currentTime / music.duration) * 100).toString()
   }
 }
 
 /**When the music slider change it will change the music duration */
-duration.onchange = () => {
+duration.onchange = (): void => {
   music.currentTime = (Number(duration.value) * music.duration) / 100
 }
 
 /**When the music is ended change the data to the next music */
-music.onended = () => {
+music.onended = (): void => {
   index++
   changeData(index)
 }
 
 /**When the volume slider change it will change the music volume */
-volume.onchange = () => {
+volume.onchange = (): void => {
   music.volume = Number(volume.value) / 100
 }
 
 /**OnClick event */
 /**Detect when we click on the play-pause button */
-play_pause.addEventListener('click', () => {
+play_pause.addEventListener('click', (): void => {
   if (music.duration > 0 && !music.paused) {
     music.pause()
   } else {
@@ -89,7 +89,7 @@ play_pause.addEventListener('click', () => {
 })
 
 /**Detect when we click on the forward button and change the data to next music */
-forward.addEventListener('click', () => {
+forward.addEventListener('click', (): void => {
   if (index + 1 === artists.length) {
     index = 0
   } else {
@@ -99,7 +99,7 @@ forward.addEventListener('click', () => {
 })
 
 /**Detect when we click on the backward button and change the data to the previous music */
-backward.addEventListener('click', () => {
+backward.addEventListener('click', (): void => {
   if (index - 1 === -1) {
     index = artists.length - 1
   } else {
